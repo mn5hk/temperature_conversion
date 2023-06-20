ef7253fe4cb6a12feacd4158746b2c6243ee9683
Alexander Savi <github@prf.simplelogin.com>
2023-06-20 12:38:49 +0200

    new function

--- conversion.py
+++ conversion.py
@@ -4,3 +4,8 @@ def celsius_to_fahrenheit(celsius):
 def celsius_to_kelvin(celsius):
     kelvin = celsius + 273.15
     return kelvin
+
+def kelvin_to_fahrenhei(kelvin):
+    whatever = 1 + 1
+    return whatever
+
