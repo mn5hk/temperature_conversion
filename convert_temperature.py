24ab2d7ebee7e4035eeb6d776e4f14d39a1032ba
mn5hk <aminshk50@gmail.com>
2023-06-21 12:36:31 +0200

    commit sphinx undate - still error with project intro

--- convert_temperature.py
+++ convert_temperature.py
@@ -1,18 +1,3 @@
-# def celsius_to_fahrenheit(celsius):
-#     return celsius/5*9 + 32
-
-# def celsius_to_kelvin(celsius):
-#     return celsius + 273.15
-
-# def kelvin_to_fahrenheit(kelvin):
-#     celsius = kelvin - 273.15
-#     fahrenheit = celsius/5*9 + 32
-#     return fahrenheit
-
-# def celsius_to_kelvin(celsius):
-#     kelvin = celsius + 273.15
-#     return kelvin
-
 def fahrenheit__(temp_in_F):
     temperature = temp_in_F
     # Convert Fahrenheit to Celsius
